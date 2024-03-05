a67bc0f1936e495af5672ed638f332d3f8cf3fee
Omar Faruqe <faruqe.cse@gmail.com>
2024-03-05 19:56:20 +0600

    Two constructor and setter getter methods added.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,48 @@
 public class HelloWorld {
+    private int roll;
+    private String name;
+    private boolean passed;
+    public HelloWorld(){ /* Constructor */
+        roll = 100;
+        name = "CSE";
+        passed = true;
+    }
+    public HelloWorld(int roll, String name, boolean passed){ /* Constructor */
+        this.roll = roll;
+        this.name = name;
+        this.passed = passed;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public void setPassed(boolean passed) {
+        this.passed = passed;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public boolean isPassed() {
+        return passed;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        HelloWorld helloWorld = new HelloWorld();
+        HelloWorld helloWorld2 = new HelloWorld(300,"SHU", true);
+        helloWorld.setName("ICE");
+        helloWorld.setRoll(400);
+        helloWorld.setPassed(false);
+        System.out.println(helloWorld2.getName());
+        System.out.println(helloWorld.getRoll());
+        System.out.println(helloWorld2.isPassed());
     }
 }
